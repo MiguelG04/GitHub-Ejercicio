9ca2b58179c647f8158367f8dd7a9a160935cdfe
MiguelG04 <miguel.galan@udea.edu.co>
2026-03-04 21:44:57 -0500

    version2

--- Version1.py
+++ Version1.py
@@ -36,72 +36,68 @@ class Sistema:
 
     def __init__(self):
         self.__registro = []
+    
+    def ingresarPaciente(self, paciente):
+        self.__registro.append(paciente)
 
-    def ingresarPaciente(self):
-
-        print("registro de paciente")
-
-        dato_nombre = input("Digite el nombre: ")
-        dato_cedula = int(input("Digite la cédula: "))
-        dato_genero = input("Digite el género: ")
-        dato_servicio = input("Digite el servicio: ")
-
-        nuevo = Paciente()
-
-        nuevo.asignarNombre(dato_nombre)
-        nuevo.asignarCedula(dato_cedula)
-        nuevo.asignarGenero(dato_genero)
-        nuevo.asignarServicio(dato_servicio)
-
-        self.__registro.append(nuevo)
-
-        print("Paciente guardado correctamente ")
-
-
-    def verDatosPaciente(self):
-
-        print("consultar paciente")
-
-        buscar_id = int(input("Ingrese la cédula a buscar: "))
-
+    def verDatosPaciente(self, cedula):
         for persona in self.__registro:
-            if persona.verCedula() == buscar_id:
-
-                print("\nPaciente encontrado:")
-                print("Nombre:", persona.verNombre())
-                print("Cédula:", persona.verCedula())
-                print("Género:", persona.verGenero())
-                print("Servicio:", persona.verServicio())
-                return
-
-        print("No existe un paciente con esa cédula ")
-
+            if persona.verCedula() == cedula:
+                return persona
+        return None
 
     def verNumeroPacientes(self):
 
         total = len(self.__registro)
         print("\nNúmero total de pacientes:", total)
 
-app = Sistema()
+def main():
 
-while True:
+    app = Sistema()
 
-    print("MENÚ")
-    print("1. Ingresar paciente nuevo")
-    print("2. Ver datos de paciente")
-    print("3. Ver número de pacientes")
-    print("4. Salir")
+    while True:
 
-    opcion = input("Seleccione una opción: ")
+        print("MENÚ")
+        print("1. Ingresar paciente nuevo")
+        print("2. Ver datos de paciente")
+        print("3. Ver número de pacientes")
 
-    if opcion == "1":
-        app.ingresarPaciente()
+        opcion = input("Seleccione una opción: ")
 
-    elif opcion == "2":
-        app.verDatosPaciente()
+        if opcion == "1":
+            print("REGISTRO DE PACIENTE")
+            dato_nombre = input("Digite el nombre: ")
+            dato_cedula = int(input("Digite la cédula: "))
+            dato_genero = input("Digite el género: ")
+            dato_servicio = input("Digite el servicio: ")
 
-    elif opcion == "3":
-        app.verNumeroPacientes()
+            nuevo = Paciente()
+            nuevo.asignarNombre(dato_nombre)
+            nuevo.asignarCedula(dato_cedula)
+            nuevo.asignarGenero(dato_genero)
+            nuevo.asignarServicio(dato_servicio)
 
-    else:
-        print("Opción inválida, intente nuevamente")
+            app.ingresarPaciente(nuevo)
+            print("Paciente guardado correctamente")
+        
+
+        elif opcion == "2":
+            buscar_id = int(input("Ingrese la cédula a buscar: "))
+            persona = app.verDatosPaciente(buscar_id)
+
+            if persona is not None:
+                print("\nPaciente encontrado:")
+                print("Nombre:", persona.verNombre())
+                print("Cédula:", persona.verCedula())
+                print("Género:", persona.verGenero())
+                print("Servicio:", persona.verServicio())
+            else:
+                print("No existe un paciente con esa cédula ")
+
+        elif opcion == "3":
+            app.verNumeroPacientes()
+
+        else:
+            print("Opción inválida, intente nuevamente")
+if __name__ == "__main__":
+    main()
